6cd39bee48c61538d4348e138c902823588adbde
2012-Bowman-Natalie <70528044+2012-Bowman-Natalie@users.noreply.github.com>
2024-05-09 12:41:28 -0700

    Update CPE301Final_Bowman_Torres.ino

--- CPE301Final_Bowman_Torres.ino
+++ CPE301Final_Bowman_Torres.ino
@@ -1,14 +1,53 @@
 /* CPE 301 Final Project: Swamp Cooler
 Team Members (2): Natalie Bowman and Jessica Torres
-Due: 5/10/2024
+Due: 5/11/2024
 */
+#define RDA 0x80
+#define TBE 0x20
+
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
 void setup() {
   // put your setup code here, to run once:
-
+  U0init(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+
+
+
+
+
+
+void U0init(unsigned long U0baud)
+{
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+unsigned char U0kbhit()
+{
+  return *myUCSR0A & RDA;
+}
+unsigned char U0getchar()
+{
+  return *myUDR0;
+}
+void U0putchar(unsigned char U0pdata)
+{
+  while(!(*myUCSR0A & TBE));
+  *myUDR0 = U0pdata;
 }
